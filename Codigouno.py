3ae6db944d9627f3d253126ff3a1bfc62a1d25d9
Yogamie :v <61318584+Yogam0101@users.noreply.github.com>
2023-08-11 11:56:21 -0500

    Add files via upload

---
+++ Codigouno.py
@@ -0,0 +1,5 @@
+Codigouno.py
+Nombreusuario= input ("Ingrese el nombre")
+DirecciónCasa= input ("Ingrese la dirección")
+Edad= int (input("Digite su edad"))
+Cantidadhijos= int (input ("Digite el número de hijos") )
---
+++ hola.py
@@ -0,0 +1,15 @@
+nombreUsario= "Sara"
+edad=34
+estatura=None
+eshinchadelequipodelmundo=True
+comidafavorita='sushicharron'
+ciudad=input ("¿Cuál es tu ciudad?")
+print  (F"Tu comida favorita es..." {} "Y vives en...")
+print(F "HOLA {nombreUsuario} tu edad es {edad}")
+salarioMensual=input("Digita tu salario=")
+deudasdelMes=input ("Digita las deudas a pagar")
+
+print (f "HOLA {Holanombredeusuario} tu edad es {edad}")
+print (f " su comida favorita es {comidafavorita} y tu vives en {ciudad}")
+
+sumaTotal=salarioMensual-deudasdelMes
